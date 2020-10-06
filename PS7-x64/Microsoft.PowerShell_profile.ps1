8b03f7a7088a8971091944f7245e0d3302d07ecd
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2020-10-06 15:33:18 -0500

    update

---
+++ PS7-x64/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,83 @@
+﻿$ErrorActionPreference = 'SilentlyContinue'
+
+#Powershell Customization
+function prompt {
+    $location = Get-Location
+    Write-Host -NoNewline "$(HOSTNAME.EXE) "                  -ForegroundColor Green
+    Write-Host -NoNewline '~'                                 -ForegroundColor Yellow
+    Write-Host -NoNewline $(Get-Location).Path.Split('\')[-1] -ForegroundColor Cyan
+    Write-Host -NoNewline ">" -ForegroundColor Green
+
+    $Adminp = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
+    $host.UI.RawUI.WindowTitle = 'Admin is ' + "$Adminp" + ' - PSVersion ' + $host.version + " - $location"
+
+    Return " "
+}
+
+<#Default
+Set-PSReadLineOption -BellStyle Audible
+Set-PSReadLineKeyHandler -Chord Tab -Function TabCompleteNext
+Set-PSReadLineKeyHandler -Chord Ctrl+Space -Function MenuComplete
+#>
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
+#Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+
+Function Set-WindowSize {
+    #Specify Window Size, Buffer, and Histor Parameters
+    [int]$Height = 30
+    [int]$Width = 140
+    [int]$Buffer = 8000
+
+    $console = $host.ui.rawui
+    $ConBuffer = $console.BufferSize
+    $ConSize = $console.WindowSize
+
+    $currWidth = $ConSize.Width
+    $currHeight = $ConSize.Height
+
+    # if height is too large, set to max allowed size
+    if ($Height -gt $host.UI.RawUI.MaxPhysicalWindowSize.Height) {
+        $Height = $host.UI.RawUI.MaxPhysicalWindowSize.Height
+    }
+
+    # if width is too large, set to max allowed size
+    if ($Width -gt $host.UI.RawUI.MaxPhysicalWindowSize.Width) {
+        $Width = $host.UI.RawUI.MaxPhysicalWindowSize.Width
+    }
+
+    # If the Buffer is wider than the new console setting, first reduce the width
+    If ($ConBuffer.Width -gt $Width ) {
+        $currWidth = $Width
+    }
+
+    # If the Buffer is higher than the new console setting, first reduce the height
+    If ($ConBuffer.Height -gt $Height ) {
+        $currHeight = $Height
+    }
+
+    # initial resizing if needed
+    $host.UI.RawUI.WindowSize = New-Object System.Management.Automation.Host.size($currWidth, $currHeight)
+
+    # Set the Buffer
+    $host.UI.RawUI.BufferSize = New-Object System.Management.Automation.Host.size($Width, $Buffer)
+
+    # Now set the WindowSize
+    $host.UI.RawUI.WindowSize = New-Object System.Management.Automation.Host.size($Width, $Height)
+}
+Set-WindowSize
+#-------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Downloads folder variable
+$Down = "$env:USERPROFILE\downloads"
+
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+    Import-Module "$ChocolateyProfile"
+}
+
+$ErrorActionPreference = 'Continue'
+Import-Module AdminToolbox
+Import-Module BetterCredentials
---
+++ README.md
@@ -0,0 +1,10 @@
+**PSPortableLight**
+
+Smaller version of PSPortable for quicker deployment where fewer tools are needed.
+
+Deploys a portable PowerShell package with often used modules.
+
+* Open an admin powershell prompt
+* Paste the contents of the Deploy-PSPortableLight.ps1 script into a powershell prompt
+
+Module Dependencies are installers required for some powershell modules.
