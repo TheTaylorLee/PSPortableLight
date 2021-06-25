d73360d40b58f17a21e8fc820f6a55ee477d4f27
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2021-06-25 02:53:54 -0500

    update

--- PS7-x64/Microsoft.PowerShell_profile.ps1
+++ PS7-x64/Microsoft.PowerShell_profile.ps1
@@ -32,10 +32,10 @@ function prompt {
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
 
 <#Default
-Set-PSReadLineOption -BellStyle Audible
-Set-PSReadLineKeyHandler -Chord Tab -Function TabCompleteNext
-Set-PSReadLineKeyHandler -Chord Ctrl+Space -Function MenuComplete
-Set-PSReadLineOption -editmode Windows
+    Set-PSReadLineOption -BellStyle Audible
+    Set-PSReadLineKeyHandler -Chord Tab -Function TabCompleteNext
+    Set-PSReadLineKeyHandler -Chord Ctrl+Space -Function MenuComplete
+    Set-PSReadLineOption -editmode Windows
 #>
 #Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 Set-PSReadLineOption -BellStyle None -EditMode Windows
@@ -110,25 +110,6 @@ Set-PSReadLineKeyHandler -Key F12 `
         [Microsoft.PowerShell.PSConsoleReadLine]::Insert(($command -join "`n"))
     }
 }
-
-#Set Colors
-Set-PSReadLineOption -Colors @{ Command = "`e[97m" }
-Set-PSReadLineOption -Colors @{ Comment = "`e[32m" }
-Set-PSReadLineOption -Colors @{ ContinuationPrompt = "`e[37m" }
-Set-PSReadLineOption -Colors @{ Emphasis = "`e[96m" }
-Set-PSReadLineOption -Colors @{ Error = "`e[91m" }
-Set-PSReadLineOption -Colors @{ Keyword = "`e[92m" }
-Set-PSReadLineOption -Colors @{ ListPredictionSelected = "`e[48;5;238m" }
-Set-PSReadLineOption -Colors @{ Member = "`e[97m" }
-Set-PSReadLineOption -Colors @{ Number = "`e[97m" }
-Set-PSReadLineOption -Colors @{ Operator = "`e[90m" }
-Set-PSReadLineOption -Colors @{ Parameter = "`e[90m" }
-Set-PSReadLineOption -Colors @{ Selection = "`e[30;47m" }
-Set-PSReadLineOption -Colors @{ String = "`e[36m" }
-Set-PSReadLineOption -Colors @{ Type = "`e[37m" }
-Set-PSReadLineOption -Colors @{ Variable = "`e[92m" }
-Set-PSReadLineOption -Colors @{ ListPrediction = "`e[38;2;39;255;0m" }
-Set-PSReadLineOption -Colors @{ InlinePrediction = "`e[38;2;133;193;233m" }
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -246,5 +227,12 @@ $Down = "$env:USERPROFILE\downloads"
 
 #Set starting directory to downloads
 Set-Location $Down
+
+#This is a work around to this issue. https://github.com/PowerShell/PowerShell/issues/14506
+#When upgrading powershell 7 versions and psreadline check to see if this has been addressed.
+reset-colors {
+    [Console]::ResetColor()
+}
+reset-colors
 
 $ErrorActionPreference = 'Continue'
