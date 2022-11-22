b663f44054b537c008d4a891ac5daf2cedd043b5
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2022-11-22 01:54:44 -0600

    Host check change supports single profile

--- PS7-x64/Microsoft.PowerShell_profile.ps1
+++ PS7-x64/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,5 @@
+#Variable script must be called first. Global variables used by other snippets will use these variables.
+
 $ErrorActionPreference = 'SilentlyContinue'
 
 . $PSScriptRoot\profile_snippets\variables.ps1
@@ -5,8 +7,7 @@ $ErrorActionPreference = 'SilentlyContinue'
 . $PSScriptRoot\profile_snippets\prompt.ps1
 . $PSScriptRoot\profile_snippets\psreadline.ps1
 . $PSScriptRoot\profile_snippets\exchangeonline.ps1
-#. $PSScriptRoot\profile_snippets\versioncheck\psportable.ps1
-. $PSScriptRoot\profile_snippets\versioncheck\psportablelight.ps1
+. $PSScriptRoot\profile_snippets\versioncheck\versioncheck.ps1
 . $PSScriptRoot\profile_snippets\exchangesnapin.ps1
 #. $PSScriptRoot\profile_snippets\experimentalfeatures.ps1
 . $PSScriptRoot\profile_snippets\installfont.ps1
